f23e12276a1c593952872cb87d69a873d3548e6c
Frank Earl <frank@earlconsult.com>
2017-07-17 23:59:20 -0400

    Undid the setgid/setuid mess for now...
    Needs to be revisited where we don't have this largely un-needed
    abstraction layer that mostly didn't make any sense if you coded
    to POSIX standards.

--- src/prot.c
+++ src/prot.c
@@ -1,21 +1,20 @@
 /* Public domain. */
 
-#include "hasshsgr.h"
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "prot.h"
 
-int prot_gid(int gid)
+// FIXME -- This is quite redundant as this is in all BSD/SYSV systems in the large...and 
+// 			doesn't need to stay once we get to first functioning cut of this with the 
+//			new framework (FCE 07-17-17)
+
+int prot_gid(gid_t gid)
 {
-#ifdef HASSHORTSETGROUPS
-  short x[2];
-  x[0] = gid; x[1] = 73; /* catch errors */
-  if (setgroups(1,x) == -1) return -1;
-#else
-  if (setgroups(1,&gid) == -1) return -1;
-#endif
   return setgid(gid); /* _should_ be redundant, but on some systems it isn't */
 }
 
-int prot_uid(int uid)
+int prot_uid(uid_t uid)
 {
   return setuid(uid);
 }
