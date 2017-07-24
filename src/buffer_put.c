4d7b3a2926734286e3685c094994b1b5df218f2e
David Sugar <tychosoft@gmail.com>
2017-07-23 20:28:43 -0400

    my initial take on this, for review & discussion

--- src/buffer_put.c
+++ src/buffer_put.c
@@ -15,7 +15,9 @@ static int allwrite(int (*op)(),int fd,const char *buf,unsigned int len)
       if (errno == error_intr) continue;
       return -1; /* note that some data may have been written */
     }
-    if (w == 0) ; /* luser's fault */
+//TODO: probably meant as a return -1??? a break????    
+	if (w == 0) 
+		return -1; /* luser's fault */
     buf += w;
     len -= w;
   }
