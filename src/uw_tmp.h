bc9be58aa1f538713156ef7742427da680bac2fe
Frank Earl <frank@earlconsult.com>
2017-07-24 22:07:12 -0400

    Additional binaries added and old makesystem cleanups for the same.

---
+++ src/uw_tmp.h
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+
+#include <utmp.h>
+
+#ifdef _PATH_UTMP
+  #define UW_TMP_UFILE _PATH_UTMP
+  #define UW_TMP_WFILE _PATH_WTMP
+#else
+  /* AIX only has UTMP_FILE */
+  #ifdef UTMP_FILE
+    #define UW_TMP_UFILE UTMP_FILE
+    #define UW_TMP_WFILE WTMP_FILE
+  #else
+    #error neither _PATH_UTMP nor UTMP_FILE defined.
+  #endif
+#endif
+typedef struct utmp uw_tmp;
