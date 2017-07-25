f98b961abe4026f0d637b2eed80d9a14d443a4e9
David Sugar <tychosoft@gmail.com>
2017-07-24 23:11:25 -0400

    bsd changes for utmpx, needs further work

--- ConfigureChecks.cmake
+++ ConfigureChecks.cmake
@@ -9,6 +9,7 @@ include(CheckTypeSize)
 check_include_file(dirent.h     HASDIRENT)
 check_include_file(poll.h		HASPOLL)
 check_include_file(sys/select.h HASSYSSELECT)
+check_include_file(utmpx.h      HASUTMPX)
 
 # Some things work by checking for it in the right header...
 check_symbol_exists(mkfifo	"sys/stat.h"  		HASMKFIFO)
