c4037b6ef580f708a16effc2cd5f895320a50285
David Sugar <tychosoft@gmail.com>
2017-08-04 09:20:39 -0400

    cleanup of execve for further testing

--- src/lib/unix/pathexec_run.c
+++ src/lib/unix/pathexec_run.c
@@ -5,7 +5,7 @@
 #include "str.h"
 #include "env.h"
 #include "pathexec.h"
-//TODO: USE #include <unistd.h> BUT EXECVP arguments wont match...
+#include <unistd.h>
 
 static stralloc tmp;
 
@@ -16,7 +16,7 @@ void pathexec_run(const char *file,const char * const *argv,const char * const *
   int savederrno;
 
   if (file[str_chr(file,'/')]) {
-    execve(file,argv,envp);
+    execve(file,(char *const*)argv,(char *const*)envp);
     return;
   }
 
@@ -33,7 +33,7 @@ void pathexec_run(const char *file,const char * const *argv,const char * const *
     if (!stralloc_cats(&tmp,file)) return;
     if (!stralloc_0(&tmp)) return;
 
-    execve(tmp.s,argv,envp);
+    execve(tmp.s,(char *const*)argv,(char*const*)envp);
     if (errno != error_noent) {
       savederrno = errno;
       if ((errno != error_acces) && (errno != error_perm) && (errno != error_isdir)) return;
